51d7bc41d4346065115a07fb8a81989a835a7924
erikbenton <erik.benton@hotmail.com>
2018-04-14 07:59:21 -0400

    moving displayDatabaseInfo to only onStart() in CatalogActivity

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -57,8 +57,6 @@ public class CatalogActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-        displayDatabaseInfo();
     }
 
     @Override
