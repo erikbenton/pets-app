aef026b888721bd1821eef8545d6339083d89ba3
erikbenton <erik.benton@hotmail.com>
2018-04-16 14:35:03 -0400

    adding URI constants to PetContract

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,14 +1,23 @@
 package com.example.android.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public final class PetContract
 {
 
+    // Constants for content
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_PETS = "pets";
+
     private PetContract() {}
 
     public static abstract class PetEntry implements BaseColumns
     {
+
+        // Complete content uri constant
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
 
         public static final String TABLE_NAME = "pets";
         public static final String _ID = BaseColumns._ID;
